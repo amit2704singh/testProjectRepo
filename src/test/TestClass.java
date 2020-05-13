35f339880b50f464a11744aa66f3e711f5645f25
amit2704singh <amit2704singh@gmail.com>
2020-05-13 22:04:40 +0530

    code remodse

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -3,9 +3,7 @@ package test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("testing---amit");
-		System.out.println("testing---amit");
-		System.out.println("testing---amit");
+		
 		System.out.println("testing---arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrmit");
 		System.out.println("testing---arrrrrrrrrrrrrrrrsfssfsfsfsfrrrrrrrrrrrrrrmit");
 		
